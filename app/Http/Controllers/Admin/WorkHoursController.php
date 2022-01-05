4f9685e5a753e19537bdbaa86292e82165a3dd6e
unknown <edi.sukakoding.com>
2022-01-06 00:51:45 +0700

    add work_hours

---
+++ app/Http/Controllers/Admin/WorkHoursController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\WorkHours;
+use Illuminate\Http\Request;
+
+class WorkHoursController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WorkHours  $workHours
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WorkHours $workHours)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WorkHours  $workHours
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WorkHours $workHours)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WorkHours  $workHours
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WorkHours $workHours)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WorkHours  $workHours
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WorkHours $workHours)
+    {
+        //
+    }
+}
