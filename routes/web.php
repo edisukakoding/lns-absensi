7551ab6cc31f003a703666cf376d10cd64e72b39
unknown <edi.sukakoding.com>
2022-01-06 22:32:35 +0700

    fix welcome page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage', ['ishome' => true]);
+    return view('welcome', ['ishome' => true]);
 });
 
 // Route::get('/dashboard', function () {
